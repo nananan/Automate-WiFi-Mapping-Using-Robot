d30f8a461d211a4de67ef4d638e0240abec5e55e
Eliana Cannella <eliana-c@live.it>
2018-11-05 21:49:44 +0100

    Added node to insert waypoint in db

--- catkin_ws/src/movement/insertWaypoints.py
+++ catkin_ws/src/movement/insertWaypoints.py
@@ -8,16 +8,30 @@ import imp
 db_man = imp.load_source('DB_Manager', '/home/wallf/git/Cinnamon/db.py')
 
 
-def goal_callback( goal_pose):
-	print(goal_pose)
+class WaypointsController:
+	def __init__(self):
+		self.db_manager = db_man.DB_Manager()
+
+		rospy.init_node('waypoint_controller', anonymous=False)
+		rospy.loginfo("Started ")
+
+		self.goal_subscriber = rospy.Subscriber('move_base_simple/goal', PoseStamped, self.goal_callback)
+
+	def goal_callback(self, goal_pose):
+		print(goal_pose)
+		pose = goal_pose.pose
+		orientation = goal_pose.orientation
+
+		record = OrderedDict([("position_x",pose.x),("position_y",pose.y),("position_z",pose.z),
+				("orientation_x": orientation.x), ("orientation_y": orientation.y), ("orientation_z": orientation.z), 
+				("orientation_w": orientation.w)])
+		self.db_manager.insert_Waypoint(record)
 
 if __name__ == '__main__':
 	try:
-		rospy.init_node('waypoint_controller', anonymous=False)
-
-	        rospy.Subscriber('move_base_simple/goal', PoseStamped, goal_callback)
-
-		rospy.loginfo("Started ")
+		waypoint_controller = WaypointsController()
+		waypoint_controller.spin() #In this application all user callbacks will be called from within the ros::spin() call. ros::spin()
+									#will not return until the node has been shutdown, either through a call to ros::shutdown() or a Ctrl-C. 
 		# Sleep to give the last log messages time to be sent
 		rospy.sleep(10)
 
