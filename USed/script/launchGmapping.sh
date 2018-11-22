4e1aa77111325019a25558486c59510af2a151b9
nananan <eliana-c@live.it>
2018-11-22 11:01:11 +0100

    modify parameters AMCL

--- USed/script/launchGmapping.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pkill mybot_rviz_gmapping.launch
-
-roslaunch mybot_description mybot_rviz_gmapping.launch 
-echo "Starting mybot_rviz_GMAPPING"
--- USed/script/launchOdometry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pkill c8_odom
-
-rosrun movement c8_odom &
-echo "Starting C8_ODOM"
